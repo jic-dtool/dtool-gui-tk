39d0d7c87fc98dbe182b44dec2fac6596f0c7b35
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-10-26 16:48:40 +0000

    Start work on tkgui module

---
+++ dtool_gui/tkgui.py
@@ -0,0 +1,83 @@
+"""Tkinter GUI code."""
+
+
+import os
+import logging
+
+import tkinter as tk
+
+logger = logging.getLogger(__file__)
+
+HOME_DIR = os.path.expanduser("~")
+
+
+class App(tk.Tk):
+    """Root tkinter application."""
+
+    def __init__(self):
+        super().__init__()
+        logger.info("Initialising dtool-gui")
+
+        self.title("dtool-gui")
+
+        # Remove ability to tear off menu on Windows and X11.
+        self.option_add("*tearOff", False)
+
+        # Create the menubar.
+        menubar = tk.Menu(self)
+
+        menu_file = tk.Menu(menubar)
+        menubar.add_cascade(menu=menu_file, label="File")
+        menu_edit = tk.Menu(menubar)
+        menubar.add_cascade(menu=menu_edit, label="Edit")
+
+        menu_file.add_command(
+            label="New dataset...",
+            accelerator="Ctrl+N",
+            command=self.new_dataset
+        )
+        menu_file.add_separator()
+        menu_file.add_command(
+            label="Quit",
+            accelerator="Ctrl+Q",
+            command=self.quit
+        )
+        menu_edit.add_command(
+            label="Edit metadata...",
+            accelerator="Ctrl+M",
+            command=self.edit_metadata
+        )
+
+        self.config(menu=menubar)
+
+        # Bind the accelerator keys.
+        self.bind_all("<Control-q>", self._quit_event)
+        self.bind_all("<Control-n>", self._new_dataset_event)
+        self.bind_all("<Control-m>", self._edit_metadata)
+
+    def _new_dataset_event(self, event):
+        self.new_dataset()
+
+    def new_dataset(self):
+        """Open window with form to create a new dataset."""
+        logger.info(self.new_dataset.__doc__)
+
+    def _edit_metadata(self, event):
+        self.edit_metadata()
+
+    def edit_metadata(self):
+        """Open window with form to edit a dataset's metadata."""
+        logger.info(self.edit_metadata.__doc__)
+
+    def _quit_event(self, event):
+        self.quit()
+
+    def quit(self):
+        """Quit the dtool-gui application."""
+        logger.info(self.quit.__doc__)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    app = App()
+    app.mainloop()
