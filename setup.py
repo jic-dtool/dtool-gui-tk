3c6ad029c9ca9e3151e204fc07e31e4dcd2a5bff
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-10-26 16:44:11 +0000

    Add missing setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-gui"
+version = "0.1.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-gui",
+    packages=["dtool_gui"],
+    version=version,
+    description="Graphical user interface for managing data using dtool",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[
+        "dtoolcore>=3.17.0",
+    ],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
