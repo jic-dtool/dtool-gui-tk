cab091185ff399001bd613b0b0543581b38af4ef
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-10-27 13:30:43 +0000

    Refactor code for generating menu to make accelerators platform specific

--- dtool_gui/tkgui.py
+++ dtool_gui/tkgui.py
@@ -42,29 +42,55 @@ class App(tk.Tk):
         menu_edit = tk.Menu(menubar)
         menubar.add_cascade(menu=menu_edit, label="Edit")
 
-        menu_file.add_command(
-            label="New dataset...",
-            accelerator="Ctrl+N",
-            command=self.new_dataset
+
+        self._add_menu_command(
+            menu=menu_file,
+            label="New file...",
+            accelerator_key="N",
+            cmd=self.new_dataset,
+            event_cmd=self._new_dataset_event
         )
         menu_file.add_separator()
-        menu_file.add_command(
+        self._add_menu_command(
+            menu=menu_file,
             label="Quit",
-            accelerator="Ctrl+Q",
-            command=self.quit
+            accelerator_key="Q",
+            cmd=self.quit,
+            event_cmd=self._quit_event
         )
-        menu_edit.add_command(
+        self._add_menu_command(
+            menu=menu_edit,
             label="Edit metadata...",
-            accelerator="Ctrl+M",
-            command=self.edit_metadata
+            accelerator_key="M",
+            cmd=self.edit_metadata,
+            event_cmd=self._edit_metadata_event
         )
 
         self.config(menu=menubar)
 
-        # Bind the accelerator keys.
-        self.bind_all("<Control-q>", self._quit_event)
-        self.bind_all("<Control-n>", self._new_dataset_event)
-        self.bind_all("<Control-m>", self._edit_metadata)
+
+    def _get_accelerator(self, key):
+        key = key.upper()
+        if self.platform == "aqua":
+            return "Cmd+{}".format(key)
+        return "Ctrl+{}".format(key)
+
+    def _get_accelerator_binding(self, key):
+        key = key.lower()
+        if self.platform == "aqua":
+            return "<Command-{}>".format(key)
+        return "<Control-{}>".format(key)
+
+    def _add_menu_command(self, menu, label, accelerator_key, cmd, event_cmd):
+        menu.add_command(
+            label=label,
+            accelerator=self._get_accelerator(accelerator_key),
+            command=cmd
+        )
+        self.bind_all(
+            self._get_accelerator_binding(accelerator_key),
+            event_cmd
+        )
 
     def _new_dataset_event(self, event):
         self.new_dataset()
@@ -73,7 +99,7 @@ class App(tk.Tk):
         """Open window with form to create a new dataset."""
         logger.info(self.new_dataset.__doc__)
 
-    def _edit_metadata(self, event):
+    def _edit_metadata_event(self, event):
         self.edit_metadata()
 
     def edit_metadata(self):
