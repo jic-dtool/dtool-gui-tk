12e48fdceee481fbc53650661a4d5250d590a878
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-11-19 12:09:10 +0000

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,12 @@ setup(
     author="Tjelvar Olsson",
     author_email="tjelvar.olsson@jic.ac.uk",
     url=url,
-    install_requires=[],
+    install_requires=[
+        "dtoolcore>=3.18.0",
+        "dtool-info",  # TODO: Should work to remove this.
+        "ruamel.yaml",
+        "jsonschema",
+    ],
     entry_points={
         'console_scripts': ['dtool-tk=dtool_gui_tk.tkgui:tkgui'],
     },
