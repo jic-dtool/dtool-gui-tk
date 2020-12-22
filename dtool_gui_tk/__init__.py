20d343158054538a4c9da6feb6f038633af0ceb1
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-12-22 16:36:58 +0000

    Update version to 0.2.0

--- dtool_gui_tk/__init__.py
+++ dtool_gui_tk/__init__.py
@@ -1,3 +1,3 @@
 """dtool_gui package."""
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 url = "https://github.com/jic-dtool/dtool-gui-tk"
-version = "0.1.1"
+version = "0.2.0"
 readme = open('README.rst').read()
 
 setup(
